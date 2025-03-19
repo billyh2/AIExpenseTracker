5f9c6671e3a8a3c63648c8d7aa7686ed1e28f055
billyh2 <billyh2@illinois.edu>
2025-03-19 16:10:27 -0500

    fixed firestore

--- AIExpenseTracker/Views/LogItemView.swift
+++ AIExpenseTracker/Views/LogItemView.swift
@@ -13,10 +13,47 @@ struct LogItemView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch horizontalSizeClass {
+        case.compact: compactView
+        default: regularView
+        }
+    }
+    
+    var compactView: some View {
+        HStack(spacing: 16) {
+            CategoryImageView(category: log.categoryEnum)
+            VStack(alignment: .leading, spacing: 8){
+                Text(log.name).font(.headline)
+                Text(log.dateText).font(.subheadline)
+            }
+            Spacer()
+            Text(log.amountText).font(.headline)
+        }
+    }
+    
+    var regularView: some View {
+        HStack(spacing: 16) {
+            CategoryImageView(category: log.categoryEnum)
+            Spacer()
+            Text(log.name)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            Text(log.dateText)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            Text(log.categoryEnum.rawValue)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    LogItemView()
+    VStack {
+        ForEach([
+            ExpenseLog(id: "1", name: "sushi", category: "Food", amount: 10, date: .now),
+            ExpenseLog(id: "2", name: "dildo", category: "Office supplies ", amount: 20, date: .now)]) { log in
+            LogItemView(log: log)
+        }
+    }.padding()
 }
---
+++ AIExpenseTracker/Views/LogListView.swift
@@ -0,0 +1,89 @@
+//
+//  LogListView.swift
+//  AIExpenseTracker
+//
+//  Created by Billy Huang on 2025/3/19.
+//
+
+import FirebaseFirestore
+import SwiftUI
+
+struct LogListView: View {
+    
+    @Binding var vm: LogListViewModel
+    @FirestoreQuery(
+        collectionPath: "logs", 
+        predicates: [.order(by: SortType.date.rawValue, descending: true)])
+    private var logs: [ExpenseLog]
+    
+    var body: some View {
+        listView
+            .sheet(item: $vm.logToEdit, onDismiss: { vm.logToEdit = nil}) { log in
+                Text("TODO: Log Form View")
+            }
+            .overlay {
+                if logs.isEmpty {
+                    Text("No Expenses data\nPlease add your expenses using the add button")
+                        .multilineTextAlignment(.center)
+                        .font(.headline)
+                        .padding()
+                }
+            }
+    }
+    
+    var listView: some View {
+        #if os(iOS)
+        List {
+            ForEach(logs) { log in
+                LogItemView(log: log)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        vm.logToEdit = log
+                    }
+                    .padding(.vertical, 4)
+            }
+            .onDelete(perform: self.onDelete)
+        }
+        .listStyle(.plain)
+        
+        #else
+        ZStack {
+            ScrollView {
+                ForEach(logs) { log in
+                    VStack {
+                        LogItemView(log: log)
+                        Divider()
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .contentShape(Rectangle())
+                    .padding(.horizontal)
+                    .onTapGesture {
+                        self.vm.logToEdit = log
+                    }
+                    .contextMenu{
+                        Button("Edit") { self.vm.logToEdit = log }
+                        Button("Delete") { vm.db.delete(log: log) }
+                    }
+                }
+            }.contentMargins(.vertical, 8, for: .scrollContent)
+        }
+        
+        #endif
+    }
+    
+    private func onDelete(with indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let log = logs[index]
+            vm.db.delete(log: log)
+        }
+    }
+}
+
+#Preview {
+    @State var vm = LogListViewModel()
+    return LogListView(vm: $vm)
+    #if os(macOS)
+        .frame(width: 700)
+    
+    #endif
+}
